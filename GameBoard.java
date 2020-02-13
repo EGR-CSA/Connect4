8f91de8f1da3b5416e0af131c7dde435fe3a8cb5
Faber <PFaber@egrps.org>
2020-02-13 13:01:23 -0500

    Method outlines added

--- GameBoard.java
+++ GameBoard.java
@@ -1,3 +1,36 @@
 public class GameBoard{
+    private final int rows = 6;
+    private final int columns = 7;
+    private String[][] gameBoard = new String[rows][columns]; 
     
+    public GameBoard(){
+        //initializes gameBoard with empty spaces
+    }
+
+    public int addPiece(int column, char player){
+        //adds the players chose piece to the correct location in the give column
+        //returns the row that the piece was added to
+        return 1;
+    }
+
+    boolean checkWin(int row, int column){
+        //checks to see if current player has won the game based on the given 
+        //row and column locations from the parameters
+        //returns true if win condition is met
+        
+        return false;
+    }
+
+    int checkInput(int column){
+        //check if the column is a valid location to place the player piece
+        //if not, ask for a valid column
+        //return a valid column
+        
+        return column;
+    }
+
+    public String toString(){
+        //return a string with the gameBoard 2D array formatted
+        //to look like a connect 4 game.
+    }
 }
