3710aa0e8b86bea2f2dc744ef8fb754596f3f475
Victor Schmitt <victorschmitt@Victors-MacBook-Pro.local>
2020-02-19 10:07:47 -0500

    Better toString

--- GameBoard.java
+++ GameBoard.java
@@ -48,9 +48,9 @@ public class GameBoard{
             for(int j = 0; j<columns; j++){
                 connect4Board += "- "+gameBoard[i][j]+" -";
             }
-            System.out.println(); 
+            connect4Board += "\n"; 
         }
-        return "| "+gameBoard[0][0]+" - "+gameBoard[0][1]+" - "+gameBoard[0][2]+" - "+gameBoard[0][3]+" - "+gameBoard[0][4]+" - "+gameBoard[0][5]+" - "+gameBoard[0][6]+" |\n"+"| "+gameBoard[1][0]+" - "+gameBoard[1][1]+" - "+gameBoard[1][2]+" - "+gameBoard[1][3]+" - "+gameBoard[1][4]+" - "+gameBoard[1][5]+" - "+gameBoard[1][6]+" |\n"+"| "+gameBoard[2][0]+" - "+gameBoard[2][1]+" - "+gameBoard[2][2]+" - "+gameBoard[2][3]+" - "+gameBoard[2][4]+" - "+gameBoard[2][5]+" - "+gameBoard[2][6]+" |\n"+"| "+gameBoard[3][0]+" - "+gameBoard[3][1]+" - "+gameBoard[3][2]+" - "+gameBoard[3][3]+" - "+gameBoard[3][4]+" - "+gameBoard[3][5]+" - "+gameBoard[3][6]+" |\n"+"| "+gameBoard[4][0]+" - "+gameBoard[4][1]+" - "+gameBoard[4][2]+" - "+gameBoard[4][3]+" - "+gameBoard[4][4]+" - "+gameBoard[4][5]+" - "+gameBoard[4][6]+" |\n"+"| "+gameBoard[5][0]+" - "+gameBoard[5][1]+" - "+gameBoard[5][2]+" - "+gameBoard[5][3]+" - "+gameBoard[5][4]+" - "+gameBoard[5][5]+" - "+gameBoard[5][6]+" |\n"+"-----------------------------";
+        return connect4Board;
     }
 
     public void rageQuit(char player){
